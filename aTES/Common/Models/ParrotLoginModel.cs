28c9e49d213ee47f0fb26f1eead724e7af02a034
Daniil Polyanskiy <daniil.polyanskiy@gmail.com>
2022-10-08 12:35:37 +0400

    added dummy authorization attribute

---
+++ aTES/Common/Models/ParrotLoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class ParrotLoginModel
+    {
+        public bool LoginSuccessful { get; set; }
+
+        public string Name { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}

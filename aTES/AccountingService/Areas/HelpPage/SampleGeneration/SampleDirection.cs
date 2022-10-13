1919c7d32f965e447ad766f5c2f6576917e54ae6
Daniil Polyanskiy <daniil.polyanskiy@gmail.com>
2022-10-13 09:43:30 +0400

    dummy plugs for analytics and accounting, package updates

---
+++ aTES/AccountingService/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace AccountingService.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ aTES/AnalyticsService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AnalyticsService.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
